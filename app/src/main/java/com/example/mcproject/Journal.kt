923a1e47f180bdc18016ed79d8962ebdac9eb0d3
Jagbir Singh <jg581261@dal.ca>
2023-11-26 17:59:48 -0400

    refresh

--- app/src/main/java/com/example/mcproject/Journal.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mcproject
-
-data class Journal(
-    val id: Int,
-    val title: String,
-    val content: String,
-    val tags: MutableList<String>,
-    val notes: MutableMap<String, String> = mutableMapOf() // Maps tag names to content
-)
