337167593525940e8d0c31191e04f5773b32148f
Sourav <sourav15102@iiitd.ac.in>
2023-11-26 11:35:38 -0400

    added back button

--- app/src/main/java/com/example/mcproject/ViewJournal.kt
+++ app/src/main/java/com/example/mcproject/ViewJournal.kt
@@ -3,6 +3,9 @@ package com.example.mcproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Button
+import android.view.View
+import android.content.Intent
 
 class ViewJournal : AppCompatActivity() {
     // define variables for the elements in activity_view_journal
@@ -18,11 +21,17 @@ class ViewJournal : AppCompatActivity() {
 
         val textTitle = findViewById<TextView>(R.id.textTitle)
         val textContent = findViewById<TextView>(R.id.textContent)
+        val backButton = findViewById<Button>(R.id.backButton)
 
         // assign text title to textTitle entity from the layout
         textTitle.text = journal.title
 
         // assign text content to textContent entity from layout.
         textContent.text = journal.content
+
+        // setup back button to go to previous activity in stack
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
